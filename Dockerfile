da5da8d63cd1af9dca4f625fe1bacba3f4b5d7b7
zberg <zberg@indeed.com>
2019-02-27 01:29:58 +0000

    oof

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,35 @@
-FROM golang:latest
+ARG GO_VERSION=1.11
+FROM golang:${GO_VERSION}-alpine AS builder
+
+RUN mkdir /user && \
+    echo 'nobody:x:65534:65534:nobody:/:' > /user/passwd && \
+    echo 'nobody:x:65534:' > /user/group
+
+RUN apk add --no-cache ca-certificates git
+
+WORKDIR /src
+
+COPY ./go.mod ./go.sum ./
+RUN go mod download
+
+COPY ./ ./
+
+RUN CGO_ENABLED=0 go build \
+    -installsuffix 'static' \
+    -o /app .
+
+FROM scratch AS final
+
+COPY --from=builder /user/group /user/passwd /etc/
+
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+
+COPY --from=builder /app /app
+
+EXPOSE 3000
+
+# Perform any further action as an unprivileged user.
+USER nobody:nobody
+
+# Run the compiled binary.
+ENTRYPOINT ["/app"]
--- main.go
+++ main.go
@@ -15,43 +15,35 @@ func ip(c echo.Context) error {
 	// 	c.Logger().Info("oops")
 	// }
 
-	payload := `<!DOCTYPE html>
+	payload := `<!doctype html>
 	<html lang="en">
 	  <head>
-		  <meta charset="utf-8">
-		  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		  
-		  <meta name="description" content="urip">
-		  <meta name="author" content="urip">
-  
-		  <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css">
-		  <link rel="stylesheet" href="https://unpkg.com/@coreui/icons/css/coreui-icons.min.css">
-		  <title>urip.fyi</title>
-	  </head> 
-	  <body class="app header-fixed bg-white">
-	  <div class="app-body">
-	  <table>
-	<tbody>
-	<tr>
-	<td>&nbsp;</td>
-	<td>&nbsp;</td>
-	<td>&nbsp;</td>
-	</tr>
-	<tr>
-	<td>&nbsp;</td>
-	<td><img src="./corgi.png" class="img-fluid" alt="Responsive image">`
-	payload += c.Request().RemoteAddr
-	payload += `</td>
-	<td>&nbsp;</td>
-	</tr>
-	<tr>
-	<td>&nbsp;</td>
-	<td>&nbsp;</td>
-	<td>&nbsp;</td>
-	</tr>
-	</tbody>
-	</table></div>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		 <title>urip.fyi</title>
+	  </head> 
+	  <body>
+	  <div class="container">
+		  <div class="row">
+		  	<div class="col-12">
+			  <div class="d-inline-block position-relative">
+			  <img src="./corgi.png" class="d-block" alt="corgi butts drive me nuts">
+			  </div>
+		  		<div class="position-relative">`
+	payload += c.Request().RemoteAddr
+	payload += `</div>
+			</div>
+		</div>
+	</div>
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>`
 
